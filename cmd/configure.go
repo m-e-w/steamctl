794f02b5c059c5a9d70eb09675932bce1e6fcbe6
m-e-w <walls.e.matthew@gmail.com>
2026-01-12 22:26:49 -0500

    added support for both steam profile url and steam id

--- cmd/configure.go
+++ cmd/configure.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 
+	"github.com/m-e-w/steamctl/internal/cli"
 	"github.com/spf13/cobra"
 )
 
@@ -32,14 +37,54 @@ Examples:
 		fmt.Scan(&steamAPIKey)
 
 		steamID := ""
-		fmt.Print("Steam ID: ")
-		fmt.Scan(&steamID)
+		steamInput := ""
+		fmt.Print("Steam profile URL (or SteamID64): ")
+		fmt.Scan(&steamInput)
 
-		configContent := fmt.Sprintf(
-			"[default]\nsteam_api_key = \"%s\"\nsteam_id = \"%s\"\n",
-			steamAPIKey,
-			steamID,
-		)
+		configContent := ""
+
+		// Take input from user and check if we get a steam id or profile url
+		steamID, steamProfileURL, err := cli.DetectSteamInput(steamInput)
+		if err != nil {
+			return err
+		}
+
+		if steamProfileURL != nil {
+			// Make the request to get the steam profile page content
+			resp, err := http.Get(steamProfileURL.String())
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+
+			// Try to find the steamid in the response
+			re := regexp.MustCompile(`"steamid":"([0-9]+)"`)
+			match := re.FindSubmatch(body)
+			if len(match) > 1 {
+				steamID = string(match[1])
+			} else {
+				return errors.New("no steamid found in steam profile page")
+			}
+
+			configContent = fmt.Sprintf(
+				"[default]\nsteam_api_key = \"%s\"\nsteam_id = \"%s\"\nsteam_profile_url = \"%s\"\n",
+				steamAPIKey,
+				steamID,
+				steamProfileURL.String(),
+			)
+		} else {
+			configContent = fmt.Sprintf(
+				"[default]\nsteam_api_key = \"%s\"\nsteam_id = \"%s\"\n",
+				steamAPIKey,
+				steamID,
+			)
+		}
+
 		configData := []byte(configContent)
 
 		home, err := os.UserHomeDir()
@@ -61,5 +106,6 @@ Examples:
 }
 
 func init() {
+	configureCmd.SilenceUsage = true
 	rootCmd.AddCommand(configureCmd)
 }
